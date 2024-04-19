980a945d26b214b55ccb6f5990f1ac0749a7c389
wisdomlesstooths <ng.ellie2001@gmail.com>
2024-04-19 18:13:48 -0400

    fix logging and formatting log display

--- SessionInfo.h
+++ SessionInfo.h
@@ -6,8 +6,8 @@ class SessionInfo
 {
 public:
     SessionInfo();
-    float baselineBefore;
-    float baselineAfter;
+    double baselineBefore;
+    double baselineAfter;
     void endSession();
 };
 
